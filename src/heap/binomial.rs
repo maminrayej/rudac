41736b63637cd295e97d4681fb81a783bea58441
mohamad amin rayej <m.amin.rayej@gmail.com>
2020-04-26 23:17:32 +0430

    implement pop for binomial heap

--- src/heap/binomial.rs
+++ src/heap/binomial.rs
@@ -2,7 +2,7 @@
 pub struct BinomialTree<T: std::cmp::Ord> {
     rank: usize,
     children: Vec<Option<BinomialTree<T>>>,
-    payload: T,
+    payload: Option<T>,
 }
 
 impl<T: std::cmp::Ord> BinomialTree<T> {
@@ -10,13 +10,13 @@ impl<T: std::cmp::Ord> BinomialTree<T> {
         BinomialTree {
             rank: 0,
             children: Vec::new(),
-            payload,
+            payload: Some(payload),
         }
     }
 
     pub fn add(&mut self, binomial_tree: BinomialTree<T>) {
         self.children.push(Some(binomial_tree));
-        self.rank *= 2;
+        self.rank += 1;
     }
 
     pub fn merge(
@@ -26,8 +26,7 @@ impl<T: std::cmp::Ord> BinomialTree<T> {
         if binomial_tree_1.rank() != binomial_tree_2.rank {
             panic!("Binomial tree ranks must be the same when merging");
         }
-
-        if binomial_tree_1.payload() <= binomial_tree_2.payload() {
+        if binomial_tree_1.is_smaller_or_equall(&binomial_tree_2) {
             binomial_tree_1.add(binomial_tree_2);
             return binomial_tree_1;
         } else {
@@ -40,14 +39,62 @@ impl<T: std::cmp::Ord> BinomialTree<T> {
         self.rank
     }
 
-    pub fn payload(&self) -> &T {
-        &self.payload
+    pub fn get_payload(&mut self) -> T {
+        if self.payload == None {
+            panic!("Internal error: payload must not be None. Please report this bug");
+        }
+
+        self.payload.take().unwrap()
+    }
+
+    fn is_smaller_or_equall(&self, other: &BinomialTree<T>) -> bool {
+        match self.peek_payload() {
+            Some(value1) => match other.peek_payload() {
+                Some(value2) => value1 <= value2,
+                None => false,
+            },
+            None => false,
+        }
+    }
+
+    pub fn peek_payload(&self) -> &Option<T> {
+        return &self.payload;
+    }
+}
+
+impl<T: std::cmp::Ord + std::fmt::Display> BinomialTree<T> {
+    fn preorder(node: &BinomialTree<T>) -> String {
+        return String::from(BinomialTree::pre_visit(&Some(node)).trim());
+    }
+
+    fn pre_visit(node: &Option<&BinomialTree<T>>) -> String {
+        let mut node_list = String::from("");
+        match node {
+            None => node_list,
+            Some(data) => {
+                // visit the node
+                match data.peek_payload() {
+                    Some(value) => node_list.push_str(format!("{} ", value).as_str()),
+                    None => (),
+                }
+                //visit children from left to right
+                for i in 0..data.children.len() {
+                    match &data.children[i] {
+                        Some(data) => {
+                            node_list.push_str(BinomialTree::pre_visit(&Some(&data)).as_str())
+                        }
+                        None => (),
+                    }
+                }
+
+                node_list
+            }
+        }
     }
 }
 
 pub struct BinomialHeap<T: std::cmp::Ord> {
     roots: Vec<Option<BinomialTree<T>>>,
-    valid: Vec<bool>,
 }
 
 impl<T: std::cmp::Ord> BinomialHeap<T> {
@@ -55,30 +102,16 @@ impl<T: std::cmp::Ord> BinomialHeap<T> {
         let root = Some(BinomialTree::init(payload));
 
         let mut roots = Vec::new();
-        let mut valid = Vec::new();
 
         roots.push(root);
-        valid.push(true);
 
-        BinomialHeap {
-            roots: roots,
-            valid,
-        }
+        BinomialHeap { roots: roots }
     }
 
-    pub fn push(&mut self, payload: T) {
-        let mut new_node = BinomialTree::init(payload);
-
-        // check if rank 0 exits
-        if !self.valid[0] {
-            self.roots[0] = Some(new_node);
-
-            return;
-        }
-
+    fn _push(&mut self, mut new_node: BinomialTree<T>, rank: usize) {
         let max_rank = self.roots.len();
 
-        for i in 0..max_rank {
+        for i in rank..max_rank {
             match self.roots[i].take() {
                 Some(node) => {
                     new_node = BinomialTree::merge(node, new_node);
@@ -87,13 +120,221 @@ impl<T: std::cmp::Ord> BinomialHeap<T> {
                         self.roots.push(Some(new_node));
                         break;
                     }
-                },
+                }
                 None => {
                     self.roots[i] = Some(new_node);
-                    
                     break;
                 }
             }
         }
+    }
+
+    pub fn push(&mut self, payload: T) {
+        let new_node = BinomialTree::init(payload);
+
+        self._push(new_node, 0);
+    }
+
+    pub fn pop(&mut self) -> T {
+        let mut min_index = 0;
+        let min_node: &Option<BinomialTree<T>>;
+
+        // find first item
+        for i in 0..self.roots.len() {
+            match &self.roots[i] {
+                Some(_) => {
+                    min_index = i;
+                }
+                None => (),
+            }
+        }
+
+        min_node = &self.roots[min_index];
+
+        // find first item
+        for i in min_index + 1..self.roots.len() {
+            match &self.roots[i] {
+                Some(node) => match min_node {
+                    Some(internal_node) => {
+                        if node.is_smaller_or_equall(internal_node) {
+                            min_index = i;
+                        }
+                    }
+                    None => (),
+                },
+                None => (),
+            }
+        }
+
+        let mut min_node_taken = self.roots[min_index].take().unwrap();
+
+        for i in 0..min_node_taken.children.len() {
+            let child = min_node_taken.children[i].take().unwrap();
+
+            let child_rank = child.rank();
+
+            self._push(child, child_rank);
+        }
+
+        return min_node_taken.get_payload();
+    }
+}
+
+impl<T: std::cmp::Ord + std::fmt::Display> BinomialHeap<T> {
+    pub fn preorder(&self) -> String {
+        let mut node_list = String::from("");
+
+        for i in 0..self.roots.len() {
+            node_list.push_str(format!("Rank {}: ", i).as_str());
+
+            match &self.roots[i] {
+                Some(data) => node_list.push_str(BinomialTree::preorder(&data).as_str()),
+                None => (),
+            }
+
+            node_list.push_str("\n");
+        }
+
+        node_list
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn binomial_tree_create() {
+        let bt = BinomialTree::init(0);
+
+        assert_eq!(*bt.peek_payload(), Some(0));
+        assert_eq!(bt.rank, 0);
+        assert_eq!(bt.children.len(), 0);
+        assert_eq!(BinomialTree::preorder(&bt), String::from("0"));
+    }
+
+    #[test]
+    fn binomial_tree_merge_rank_0() {
+        let bt1 = BinomialTree::init(0);
+        let bt2 = BinomialTree::init(1);
+
+        let merged_tree = BinomialTree::merge(bt1, bt2);
+
+        assert_eq!(BinomialTree::preorder(&merged_tree), String::from("0 1"));
+
+        assert_eq!(merged_tree.rank(), 1);
+    }
+
+    #[test]
+    fn binomial_tree_merge_rank_1() {
+        let bt1 = BinomialTree::init(0);
+        let bt2 = BinomialTree::init(1);
+
+        let merged_tree_1 = BinomialTree::merge(bt1, bt2);
+        let bt3 = BinomialTree::init(2);
+        let bt4 = BinomialTree::init(3);
+
+        let merged_tree_2 = BinomialTree::merge(bt3, bt4);
+
+        let merged_tree = BinomialTree::merge(merged_tree_1, merged_tree_2);
+
+        assert_eq!(
+            BinomialTree::preorder(&merged_tree),
+            String::from("0 1 2 3")
+        );
+        assert_eq!(merged_tree.rank(), 2);
+    }
+
+    #[test]
+    fn binomial_tree_merge_rank_2() {
+        let bt1 = BinomialTree::init(0);
+        let bt2 = BinomialTree::init(1);
+
+        let merged_tree_1 = BinomialTree::merge(bt1, bt2);
+        let bt3 = BinomialTree::init(2);
+        let bt4 = BinomialTree::init(3);
+
+        let merged_tree_2 = BinomialTree::merge(bt3, bt4);
+
+        let merged_tree_final_1 = BinomialTree::merge(merged_tree_1, merged_tree_2);
+
+        let bt1 = BinomialTree::init(1);
+        let bt2 = BinomialTree::init(2);
+
+        let merged_tree_1 = BinomialTree::merge(bt1, bt2);
+        let bt3 = BinomialTree::init(5);
+        let bt4 = BinomialTree::init(6);
+
+        let merged_tree_2 = BinomialTree::merge(bt3, bt4);
+
+        let merged_tree_final_2 = BinomialTree::merge(merged_tree_1, merged_tree_2);
+
+        let merged_tree = BinomialTree::merge(merged_tree_final_1, merged_tree_final_2);
+
+        assert_eq!(
+            BinomialTree::preorder(&merged_tree),
+            String::from("0 1 2 3 1 2 5 6")
+        );
+        assert_eq!(merged_tree.rank(), 3);
+    }
+
+    #[test]
+    fn binomial_heap_init() {
+        let bh = BinomialHeap::init(0);
+
+        assert_eq!(bh.preorder(), format!("Rank 0: 0\n"))
+    }
+
+    #[test]
+    fn binomial_heap_push_1() {
+        let mut bh = BinomialHeap::init(0);
+
+        bh.push(1);
+
+        assert_eq!(bh.preorder(), format!("Rank 0: \nRank 1: 0 1\n"));
+    }
+
+    #[test]
+    fn binomial_heap_push_2() {
+        let mut bh = BinomialHeap::init(0);
+
+        bh.push(1);
+        bh.push(2);
+
+        assert_eq!(bh.preorder(), format!("Rank 0: 2\nRank 1: 0 1\n"));
+    }
+
+    #[test]
+    fn binomial_heap_push_3() {
+        let mut bh = BinomialHeap::init(0);
+
+        bh.push(1);
+        bh.push(2);
+
+        bh.push(3);
+
+        assert_eq!(
+            bh.preorder(),
+            format!("Rank 0: \nRank 1: \nRank 2: 0 1 2 3\n")
+        );
+    }
+
+    #[test]
+    fn binomial_heap_pop() {
+        let mut bh = BinomialHeap::init(0);
+
+        bh.push(1);
+        bh.push(2);
+
+        bh.push(3);
+
+        let value = bh.pop();
+
+        assert_eq!(
+            bh.preorder(),
+            format!("Rank 0: 1\nRank 1: 2 3\nRank 2: \n")
+        );
+
+        assert_eq!(value, 0)    
     }
 }
