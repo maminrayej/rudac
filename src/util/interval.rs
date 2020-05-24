4079e1d88aa5cfe92f28be3afab1b4ce4e564b57
mohamad amin rayej <m.amin.rayej@gmail.com>
2020-05-25 03:18:34 +0430

    add test cases and debug for interval

--- src/util/interval.rs
+++ src/util/interval.rs
@@ -41,18 +41,15 @@ impl<T: Ord> Interval<T> {
     }
 
     pub fn overlaps(first: &Interval<T>, second: &Interval<T>) -> bool {
-        
         let high: &Bound<T>;
         let low: &Bound<T>;
 
         if *first == *second {
             return true;
-        }
-        else if first < second {
+        } else if first < second {
             low = second.low();
             high = first.high();
-        }
-        else {
+        } else {
             low = first.low();
             high = second.high();
         }
@@ -62,7 +59,7 @@ impl<T: Ord> Interval<T> {
             (Included(_low), Excluded(_high)) => _high > _low,
             (Excluded(_low), Included(_high)) => _high > _low,
             (Excluded(_low), Excluded(_high)) => _high > _low,
-            _ => true
+            _ => true,
         }
     }
 
@@ -71,59 +68,84 @@ impl<T: Ord> Interval<T> {
             let overlap = Interval::get_overlap(first, second).unwrap();
 
             overlap == *second
-        }
-        else {
+        } else {
             false
         }
     }
 
     pub fn get_overlap(first: &Interval<T>, second: &Interval<T>) -> Option<Interval<T>> {
-
         if !Interval::overlaps(first, second) {
             return None;
         }
 
         let low = match (&first.low(), &second.low()) {
             (Included(low1), Included(low2))
-            | (Included(low1), Excluded(low2))
+            | (Excluded(low1), Included(low2))
             | (Excluded(low1), Excluded(low2)) => {
-                if low1 <= low2 {
+                if low1 >= low2 {
                     Rc::clone(&first.low)
                 } else {
                     Rc::clone(&second.low)
                 }
             }
-            (Excluded(low1), Included(low2)) => {
-                if low1 < low2 {
+            (Included(low1), Excluded(low2)) => {
+                if low1 > low2 {
                     Rc::clone(&first.low)
                 } else {
                     Rc::clone(&second.low)
                 }
             }
-            (Unbounded, _) | (_, Unbounded) => Rc::new(Unbounded),
+            (Unbounded, Included(_)) | (Unbounded, Excluded(_)) => Rc::clone(&second.low),
+            (Included(_), Unbounded) | (Excluded(_), Unbounded) => Rc::clone(&first.low),
+
+            (Unbounded, Unbounded) => Rc::new(Unbounded),
         };
 
         let high = match (&first.high(), &second.high()) {
-            (Included(high1), Excluded(high2)) => {
-                if high1 < high2 {
-                    Rc::clone(&first.low)
-                } else {
-                    Rc::clone(&second.low)
-                }
-            }
             (Included(high1), Included(high2))
             | (Excluded(high1), Included(high2))
             | (Excluded(high1), Excluded(high2)) => {
                 if high1 <= high2 {
-                    Rc::clone(&first.low)
+                    Rc::clone(&first.high)
                 } else {
-                    Rc::clone(&second.low)
+                    Rc::clone(&second.high)
                 }
             }
-            (Unbounded, _) | (_, Unbounded) => Rc::new(Unbounded),
+            (Included(high1), Excluded(high2)) => {
+                if high1 < high2 {
+                    Rc::clone(&first.high)
+                } else {
+                    Rc::clone(&second.high)
+                }
+            }
+            (Unbounded, Included(_)) | (Unbounded, Excluded(_)) => Rc::clone(&second.high),
+            (Included(_), Unbounded) | (Excluded(_), Unbounded) => Rc::clone(&first.high),
+
+            (Unbounded, Unbounded) => Rc::new(Unbounded),
         };
 
         Some(Interval { low, high })
+    }
+}
+
+impl<T: Ord + std::fmt::Display> std::fmt::Display for Interval<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let low: String;
+        let high: String;
+
+        low = match &self.low() {
+            Included(low) => format!("[{}", low),
+            Excluded(low) => format!("({}", low),
+            Unbounded => format!("(_"),
+        };
+
+        high = match &self.high() {
+            Included(high) => format!("{}]", high),
+            Excluded(high) => format!("{})", high),
+            Unbounded => format!("_)"),
+        };
+
+        write!(f, "{},{}", low, high)
     }
 }
 
@@ -216,10 +238,12 @@ impl<T: Ord> PartialOrd for Interval<T> {
                         Some(Ordering::Greater)
                     }
                 }
-                (Unbounded, Included(_)) => Some(Ordering::Less),
-                (Unbounded, Excluded(_)) => Some(Ordering::Less),
-                (Included(_), Unbounded) => Some(Ordering::Greater),
-                (Excluded(_), Unbounded) => Some(Ordering::Greater),
+                (Unbounded, Included(_)) => Some(Ordering::Greater),
+                (Unbounded, Excluded(_)) => Some(Ordering::Greater),
+
+                (Included(_), Unbounded) => Some(Ordering::Less),
+                (Excluded(_), Unbounded) => Some(Ordering::Less),
+
                 (Unbounded, Unbounded) => Some(Ordering::Equal),
             };
         }
@@ -239,26 +263,282 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_bound_1() {
-        let bound1 = Bound::Included(1);
-        let bound2 = Bound::Excluded(1);
-        let bound3 = Bound::Included(1);
-        let bound4 = Bound::Included(2);
+    fn util_interval_valid_1() {
+        assert!(Interval::valid(&Interval::new(Included(2), Included(2))));
+        assert!(Interval::valid(&Interval::new(Excluded(2), Included(3))));
+        assert!(Interval::valid(&Interval::new(Included(2), Excluded(3))));
+        assert!(Interval::valid(&Interval::new(Excluded(2), Excluded(3))));
 
-        assert_eq!(bound1 == bound2, false);
-        assert_eq!(bound1 == bound3, true);
-        assert_eq!(bound3 == bound4, false);
+        assert!(Interval::valid(&Interval::new(Unbounded, Included(1))));
+        assert!(Interval::valid(&Interval::new(Excluded(2), Unbounded)));
+        assert!(Interval::<usize>::valid(&Interval::new(
+            Unbounded, Unbounded
+        )));
     }
 
     #[test]
-    fn test_bound_2() {
-        let bound1 = Bound::Included(String::from("amin"));
-        let bound2 = Bound::Excluded(String::from("amin"));
-        let bound3 = Bound::Included(String::from("amin"));
-        let bound4 = Bound::Included(String::from("amin2"));
+    #[should_panic(expected = "Interval is not valid")]
+    fn util_interval_panic_new_1() {
+        assert!(!Interval::valid(&Interval::new(Included(2), Included(1))));
+    }
 
-        assert_eq!(bound1 == bound2, false);
-        assert_eq!(bound1 == bound3, true);
-        assert_eq!(bound3 == bound4, false);
+    #[test]
+    #[should_panic(expected = "Interval is not valid")]
+    fn util_interval_panic_new_2() {
+        assert!(!Interval::valid(&Interval::new(Excluded(2), Included(1))));
+    }
+
+    #[test]
+    #[should_panic(expected = "Interval is not valid")]
+    fn util_interval_panic_new_3() {
+        assert!(!Interval::valid(&Interval::new(Included(2), Excluded(1))));
+    }
+
+    #[test]
+    #[should_panic(expected = "Interval is not valid")]
+    fn util_interval_panic_new_4() {
+        assert!(!Interval::valid(&Interval::new(Excluded(2), Excluded(1))));
+    }
+
+    #[test]
+    fn util_interval_compare_1() {
+        let interval1 = Interval::new(Included(2), Included(3));
+        let interval2 = Interval::new(Included(2), Excluded(3));
+        let interval3 = Interval::new(Excluded(2), Included(3));
+        let interval4 = Interval::new(Excluded(2), Excluded(3));
+
+        let interval5 = Interval::new(Unbounded, Excluded(3));
+        let interval6 = Interval::new(Excluded(2), Unbounded);
+        let interval7 = Interval::new(Unbounded, Excluded(3));
+        let interval8 = Interval::<usize>::new(Unbounded, Unbounded);
+
+        assert!(interval1 == interval1);
+        assert!(interval1 > interval2);
+        assert!(interval2 < interval1);
+        assert!(interval2 < interval3);
+        assert!(interval3 > interval4);
+        assert!(interval5 < interval6);
+        assert!(interval7 < interval6);
+        assert!(interval5 < interval8);
+        assert!(interval6 > interval8);
+    }
+
+    #[test]
+    fn util_interval_overlaps_1() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Included(2), Included(4));
+
+        assert!(Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_2() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Included(2), Excluded(3));
+
+        assert!(Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_3() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Excluded(1), Excluded(3));
+
+        assert!(Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_4() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Excluded(3), Excluded(4));
+
+        assert!(!Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_5() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Excluded(0), Excluded(1));
+
+        assert!(!Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_6() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Excluded(4), Excluded(5));
+
+        assert!(!Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_7() {
+        let interval1 = Interval::new(Unbounded, Included(3));
+        let interval2 = Interval::new(Excluded(1), Excluded(5));
+
+        assert!(Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn util_interval_overlaps_8() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Excluded(4), Unbounded);
+
+        assert!(!Interval::overlaps(&interval1, &interval2));
+    }
+
+    #[test]
+    fn until_interval_get_overlap_1() {
+        let interval1 = Interval::new(Included(1), Included(3));
+        let interval2 = Interval::new(Included(2), Included(4));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Included(2), Included(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_2() {
+        let interval1 = Interval::new(Included(1), Excluded(3));
+        let interval2 = Interval::new(Included(2), Included(4));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Included(2), Excluded(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_3() {
+        let interval1 = Interval::new(Included(1), Excluded(3));
+        let interval2 = Interval::new(Included(2), Included(3));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Included(2), Excluded(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_4() {
+        let interval1 = Interval::new(Excluded(1), Excluded(3));
+        let interval2 = Interval::new(Included(1), Included(4));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Excluded(1), Excluded(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_5() {
+        let interval1 = Interval::new(Unbounded, Excluded(3));
+        let interval2 = Interval::new(Included(1), Included(2));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Included(1), Included(2))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_6() {
+        let interval1 = Interval::new(Unbounded, Excluded(3));
+        let interval2 = Interval::new(Unbounded, Included(2));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Unbounded, Included(2))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_7() {
+        let interval1 = Interval::new(Excluded(2), Excluded(3));
+        let interval2 = Interval::new(Unbounded, Included(3));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Excluded(2), Excluded(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_8() {
+        let interval1 = Interval::new(Excluded(2), Unbounded);
+        let interval2 = Interval::new(Unbounded, Unbounded);
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Excluded(2), Unbounded)
+        );
+    }
+
+    #[test]
+    fn until_interval_get_overlap_9() {
+        let interval1 = Interval::new(Excluded(2), Included(3));
+        let interval2 = Interval::new(Excluded(3), Included(4));
+
+        assert!(Interval::get_overlap(&interval1, &interval2).is_none());
+    }
+
+    #[test]
+    fn until_interval_get_overlap_10() {
+        let interval1 = Interval::new(Excluded(2), Included(3));
+        let interval2 = Interval::new(Included(4), Included(4));
+
+        assert!(Interval::get_overlap(&interval1, &interval2).is_none());
+    }
+
+    #[test]
+    fn until_interval_get_overlap_11() {
+        let interval1 = Interval::new(Included(3), Included(4));
+        let interval2 = Interval::new(Included(2), Included(3));
+
+        assert!(
+            Interval::get_overlap(&interval1, &interval2).unwrap()
+                == Interval::new(Included(3), Included(3))
+        );
+    }
+
+    #[test]
+    fn until_interval_contains_1() {
+        let interval1 = Interval::new(Included(1), Included(4));
+        let interval2 = Interval::new(Included(2), Included(3));
+
+        assert!(Interval::contains(&interval1, &interval2));
+    }
+
+    #[test]
+    fn until_interval_contains_2() {
+        let interval1 = Interval::new(Included(1), Included(4));
+        let interval2 = Interval::new(Excluded(1), Included(3));
+
+        assert!(Interval::contains(&interval1, &interval2));
+    }
+
+    #[test]
+    fn until_interval_contains_3() {
+        let interval1 = Interval::new(Included(1), Included(4));
+        let interval2 = Interval::new(Included(1), Included(3));
+
+        assert!(Interval::contains(&interval1, &interval2));
+    }
+
+    #[test]
+    fn until_interval_contains_4() {
+        let interval1 = Interval::new(Included(1), Included(4));
+        let interval2 = Interval::new(Included(2), Excluded(4));
+
+        assert!(Interval::contains(&interval1, &interval2));
+    }
+
+    #[test]
+    fn until_interval_contains_5() {
+        let interval1 = Interval::new(Included(1), Included(4));
+        let interval2 = Interval::new(Included(2), Included(4));
+
+        assert!(Interval::contains(&interval1, &interval2));
     }
 }
